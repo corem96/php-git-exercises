0ce8d4d3f7f30094e7b28eac62dc504ff0515f56
Jorge Escamilla <corem96@gmail.com>
2018-09-14 02:11:19 -0600

    Loads twig to enviroment. Creates a BookModel and displays book info

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
+use Bookstore\Core\Db;;
+use Bookstore\Models\BookModel;
+
 require_once __DIR__ . '/vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem(__DIR__ . '\views');
+$twig = new Twig_Environment($loader);
+
+$bookModel = new BookModel(Db::getInstance());
+$book = $bookModel->get(1);
+
+$params = ['book' => $book];
+echo $twig->loadTemplate('book.twig')->render($params);
