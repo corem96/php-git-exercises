fc5bc5473c52006632e8347e82d77028148fd64c
Jorge Escamilla <corem96@gmail.com>
2018-09-14 22:49:26 -0600

    New saleModel instance to render in twig view

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 use Bookstore\Core\Db;;
 use Bookstore\Models\BookModel;
+use Bookstore\Models\SaleModel;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
@@ -9,7 +10,13 @@ $loader = new Twig_Loader_Filesystem(__DIR__ . '\views');
 $twig = new Twig_Environment($loader);
 
 $bookModel = new BookModel(Db::getInstance());
-$book = $bookModel->get(1);
+$books = $bookModel->getAll(1, 3);
 
-$params = ['book' => $book];
-echo $twig->loadTemplate('book.twig')->render($params);
+$params = ['book' => $books, 'currentPage' => 2];
+echo $twig->loadTemplate('books.twig')->render($params);
+
+$saleModel = new SaleModel(Db::getInstance());
+$sales = $saleModel->getByUser(1);
+
+$params = ['sales' => $sales];
+echo $twig->loadTemplate('sales.twig')->render($params);
